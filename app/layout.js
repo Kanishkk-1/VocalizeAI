2ec7f460c33ca04f8248dae112a747dfdd706447
Kanishk <kanishka.ranjan.9@gmail.com>
2025-06-17 15:49:55 +0530

    add different agents voice

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Persoanl AI Voice Assitant',
+  title: 'Personal AI Voice Assitant',
   description: 'Voice-to-Voice assistant',
 }
 
